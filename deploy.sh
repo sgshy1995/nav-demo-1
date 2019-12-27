c0af88f7923bb507841afdc696b02287964fbcd2
Singlesaul <singlesaulwork@gmail.com>
2019-12-27 21:33:35 +0800

    update

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-yarn build &&
-git checkout gh-pages &&
-rm -rf src tests css js public *.json *.ico *.js *.css *.html *.png *.jpg *.gif *.jpeg *.lock *.sh *.md &&
-mv dist/* ./ &&
-rm -rf dist;
-git add . &&
-git commit . -m "update" &&
-git push --set-upstream origin gh-pages &&
-git checkout -
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/nav-demo-1/'
-    : '/'
-}
